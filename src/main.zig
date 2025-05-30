8227600b46de673c4c9111bfb3cff154917bfd8e
mtso <8432061+mtso@users.noreply.github.com>
2025-05-29 23:02:07 -0700

    feat: flush single log lines

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,4 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+const std = @import("std");
 
 fn match(haystack: [][]u8, needle: []const u8) bool {
     for (haystack) |line| {
@@ -24,12 +22,33 @@ fn closing(start: []const u8, end: []const u8) bool {
     };
 }
 
-pub fn main() !void {
-    const needle = "SUSHI";
+fn startsMultiline(line: []const u8) bool {
+    // start of error stacktraces
+    if (std.mem.indexOf(u8, line, "Error") != null) {
+        return true;
+    }
+    if (std.mem.indexOf(u8, line, "Exception") != null) {
+        return true;
+    }
+    // start of serialized objects
+    return switch (line[line.len - 1]) {
+        '{', '[', '(' => true,
+        else => false,
+    };
+}
 
+pub fn main() !u8 {
     const allocator = std.heap.page_allocator;
     var stdin = std.io.getStdIn().reader();
     var stdout = std.io.getStdOut().writer();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    const needle = if (args.len >= 2) args[1] else {
+        try std.io.getStdErr().writeAll("usage: lgrep [pattern]\n");
+        return 1;
+    };
 
     var line_buffer = std.ArrayList(u8).init(allocator);
     defer line_buffer.deinit();
@@ -50,17 +69,40 @@ pub fn main() !void {
 
         if (read_result.?.len > 0) {
             if (line_cache.items.len == 0) {
-                try line_cache.append(read_result.?);
+                if (startsMultiline(read_result.?)) {
+                    // start of multiline log
+                    try line_cache.append(read_result.?);
+                } else {
+                    // single line log
+                    if (std.mem.indexOf(u8, read_result.?, needle) != null) {
+                        try stdout.writeAll(read_result.?);
+                        try stdout.writeAll("\n");
+                        allocator.free(read_result.?);
+                    }
+                }
             } else if (read_result.?[0] == ' ') {
-                try line_cache.append(read_result.?);
+                if (line_cache.items.len > 0) {
+                    // continues multiline log
+                    try line_cache.append(read_result.?);
+                } else if (startsMultiline(read_result.?)) {
+                    // no existing multiline log, but begins a multiline log
+                    try line_cache.append(read_result.?);
+                } else {
+                    // no existing multiline log
+                    // single line log
+                    if (std.mem.indexOf(u8, read_result.?, needle) != null) {
+                        try stdout.writeAll(read_result.?);
+                        try stdout.writeAll("\n");
+                        allocator.free(read_result.?);
+                    }
+                }
             } else {
                 const check_line = line_cache.items[check_index];
                 if (closing(check_line, read_result.?)) {
                     try line_cache.append(read_result.?);
                     check_index = line_cache.items.len - 1;
                 } else {
-                    // assume not the same, reset
-
+                    // assume the line is a part of a new log, reset
                     if (match(line_cache.items, needle)) {
                         for (line_cache.items) |l| {
                             try stdout.writeAll(l);
@@ -74,7 +116,16 @@ pub fn main() !void {
                     }
                     line_cache.clearRetainingCapacity();
                     check_index = 0;
-                    try line_cache.append(read_result.?);
+                    if (startsMultiline(read_result.?)) {
+                        try line_cache.append(read_result.?);
+                    } else {
+                        // single line log
+                        if (std.mem.indexOf(u8, read_result.?, needle) != null) {
+                            try stdout.writeAll(read_result.?);
+                            try stdout.writeAll("\n");
+                            allocator.free(read_result.?);
+                        }
+                    }
                 }
             }
         } else {
@@ -96,6 +147,6 @@ pub fn main() !void {
     }
     line_cache.clearRetainingCapacity();
     check_index = 0;
-}
 
-const std = @import("std");
+    return 0;
+}
